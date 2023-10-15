3e924ba216be1966e5d4755f995afc2e2084aa32
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2023-10-15 23:23:28 +0300

    Вынес копипасту в функцию

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Pokemon, PokemonEntity
 
-
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
     'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
@@ -12,18 +11,16 @@ DEFAULT_IMAGE_URL = (
     '&fill=transparent'
 )
 
+def get_image_url(request, image):
+    if image:
+        return request.build_absolute_uri(image.url)
+    else:
+        return DEFAULT_IMAGE_URL
+
 
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
-    icon = folium.features.CustomIcon(
-        image_url,
-        icon_size=(50, 50),
-    )
-    folium.Marker(
-        [lat, lon],
-        # Warning! `tooltip` attribute is disabled intentionally
-        # to fix strange folium cyrillic encoding bug
-        icon=icon,
-    ).add_to(folium_map)
+    icon = folium.features.CustomIcon(image_url, icon_size=(50, 50))
+    folium.Marker([lat, lon], icon=icon).add_to(folium_map)
 
 
 def show_all_pokemons(request):
@@ -34,11 +31,7 @@ def show_all_pokemons(request):
         disappeared_at__gte=time_now
     )
     for pokemon_entity in pokemon_entities:
-        pokemon_image = pokemon_entity.pokemon.image
-        if pokemon_image:
-            image_url = request.build_absolute_uri(pokemon_image.url)
-        else:
-            image_url = DEFAULT_IMAGE_URL
+        image_url = get_image_url(request, pokemon_entity.pokemon.image)
         add_pokemon(
             folium_map,
             pokemon_entity.latitude,
@@ -49,12 +42,7 @@ def show_all_pokemons(request):
     pokemons = Pokemon.objects.all()
     pokemons_on_page = []
     for pokemon in pokemons:
-        if pokemon.image:
-            image_url = request.build_absolute_uri(
-                pokemon.image.url
-            )
-        else:
-            image_url = DEFAULT_IMAGE_URL
+        image_url = get_image_url(request, pokemon.image)
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
             'img_url': image_url,
@@ -70,10 +58,7 @@ def show_all_pokemons(request):
 def show_pokemon(request, pokemon_id):
     pokemon = get_object_or_404(Pokemon, id=pokemon_id)
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    if pokemon.image:
-        image_url = request.build_absolute_uri(pokemon.image.url)
-    else:
-        image_url = DEFAULT_IMAGE_URL
+    image_url = get_image_url(request, pokemon.image)
     time_now = timezone.now()
     pokemon_entities = PokemonEntity.objects.filter(
         pokemon=pokemon,
@@ -93,17 +78,14 @@ def show_pokemon(request, pokemon_id):
         previous_evolution = {
             "title_ru": previous_pokemon.title,
             "pokemon_id": previous_pokemon.id,
-            "img_url": request.build_absolute_uri(previous_pokemon.image.url),
-            'description': previous_pokemon.description,
-            'title_en': previous_pokemon.title_en,
-            'title_jp': previous_pokemon.title_jp,
+            "img_url": get_image_url(request, previous_pokemon.image)
         }
     next_pokemon = pokemon.next_evolutions.all().first()
     if next_pokemon:
         next_evolution = {
             "title_ru": next_pokemon.title,
             "pokemon_id": next_pokemon.id,
-            "img_url": request.build_absolute_uri(next_pokemon.image.url)
+            "img_url": get_image_url(request, next_pokemon.image)
         }
     else:
         next_evolution = {}
@@ -120,3 +102,4 @@ def show_pokemon(request, pokemon_id):
             'next_evolution': next_evolution,
         }
     })
+
